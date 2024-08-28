63d8713603aa58d6b1f9fa1fb845deaa8709daea
Nikolas Martens <nikolas.m@rtens.org>
2024-08-29 00:24:45 +0200

    Never quit

--- status.py
+++ status.py
@@ -85,9 +85,10 @@ except Exception:
 
 threshold = 1
 
+status = bcolors.end
 thread = None
 switch = None
-logins = 1
+logins = 0
 
 try:
     with open("login.json") as f:
@@ -101,110 +102,114 @@ except Exception:
         json.dump(login, f)
 
 api = growattServer.GrowattApi(False, "MIC55555")
-login_response = api.login(login["usr"], login["pwd"])
-
-if not login_response["success"]:
-    log("login error: " + login_response["error"])
-    shutdown()
 
 while True:
     try:
-        plants = api.plant_list(login_response['user']['id'])
-    except Exception as e:
         logins += 1
-        log("login again: " + str(e))
-
         login_response = api.login(login["usr"], login["pwd"])
+
         if not login_response["success"]:
-            log("login error: " + login_response["error"])
-            shutdown()
+            raise Exception(login_response["error"])
 
         plants = api.plant_list(login_response['user']['id'])
 
-    plant_id = plants['data'][0]["plantId"]
-    plant_info = api.plant_info(plant_id)
-    device_sn = plant_info["deviceList"][0]["deviceSn"]
-    mix_status = api.mix_system_status(device_sn, plant_id)
+        plant_id = plants['data'][0]["plantId"]
+        plant_info = api.plant_info(plant_id)
+        device_sn = plant_info["deviceList"][0]["deviceSn"]
+        mix_status = api.mix_system_status(device_sn, plant_id)
 
-    history.append(round(float(mix_status["ppv"]) - float(mix_status["pLocalLoad"]), 2))
-    if len(history) > 7: history.pop(0)
+        history.append(round(float(mix_status["ppv"]) - float(mix_status["pLocalLoad"]), 2))
+        if len(history) > 7: history.pop(0)
 
-    with open("history.json", "w") as f:
-        json.dump(history, f)
+        with open("history.json", "w") as f:
+            json.dump(history, f)
 
-    average_net = round(sum(history) / len(history), 2)
+        average_net = round(sum(history) / len(history), 2)
 
-    soc = float(mix_status["SOC"])
+        soc = float(mix_status["SOC"])
 
-    if average_net >= threshold:
-        threshold = 0
-        status = bcolors.green
+        if average_net >= threshold:
+            threshold = 0
+            status = bcolors.green
 
-        if switch == False or switch == None:
-            switch = True
-            log("switched on")
+            if switch == False or switch == None:
+                switch = True
+                log("switched on")
 
-        if soc >= 95:
-            thread = threading.Thread(target=led_set, args=(True,))
+            if soc >= 95:
+                thread = threading.Thread(target=led_set, args=(True,))
+            else:
+                thread = threading.Thread(target=led_flash, args=(2, .1))
         else:
-            thread = threading.Thread(target=led_flash, args=(2, .1))
-    else:
-        if switch == True or switch == None:
-            switch = False
-            log("switched off")
+            if switch == True or switch == None:
+                switch = False
+                log("switched off")
 
-        if average_net >= 0:
-            status = bcolors.blue
-            thread = threading.Thread(target=led_flash, args=(1, .5))
-        elif soc >= 15:
-            threshold = .5
-            status = bcolors.yellow
-            thread = threading.Thread(target=led_flash, args=(1, 2))
-        else:
-            threshold = 1
-            status = bcolors.red
-            thread = threading.Thread(target=led_flash, args=(.1, 5))
+            if average_net >= 0:
+                status = bcolors.blue
+                thread = threading.Thread(target=led_flash, args=(1, .5))
+            elif soc >= 15:
+                threshold = .5
+                status = bcolors.yellow
+                thread = threading.Thread(target=led_flash, args=(1, 2))
+            else:
+                threshold = 1
+                status = bcolors.red
+                thread = threading.Thread(target=led_flash, args=(.1, 5))
 
-    print(bcolors.hide, end="")
-    print(status, end="")
-    print("Updated:    ", datetime.datetime.now().strftime("%H:%M:%S"))
-    print("Logins:     ", logins)
-    print("Using:      ", mix_status["pLocalLoad"], "kW")
-    print("Generating: ", mix_status["ppv"], "kW")
-    print("Battery:    ", mix_status["SOC"], "%")
-    print("Charging:   ", mix_status["chargePower"], "kW")
-    print("Discharging:", mix_status["pdisCharge1"], "kW")
-    print("Importing:  ", mix_status["pactouser"], "kW")
-    print("Exporting:  ", mix_status["pactogrid"], "kW")
-    print("History:    ", average_net, history)
-    print("Threshold:  ", threshold)
-    print("Switch:     ", switch)
+        print(bcolors.hide, end="")
+        print(status, end="")
+        print("Updated:    ", datetime.datetime.now().strftime("%H:%M:%S"))
+        print("Logins:     ", logins)
+        print("Using:      ", mix_status["pLocalLoad"], "kW")
+        print("Generating: ", mix_status["ppv"], "kW")
+        print("Battery:    ", mix_status["SOC"], "%")
+        print("Charging:   ", mix_status["chargePower"], "kW")
+        print("Discharging:", mix_status["pdisCharge1"], "kW")
+        print("Importing:  ", mix_status["pactouser"], "kW")
+        print("Exporting:  ", mix_status["pactogrid"], "kW")
+        print("History:    ", average_net, history)
+        print("Threshold:  ", threshold)
+        print("Switch:     ", switch)
 
-    print()
-    print(multibar((("=", soc-10),), 90))
-    print(multibar((
-        ("#", mix_status["pLocalLoad"]),
-        ("+", mix_status["chargePower"]),
-        (">", mix_status["pactogrid"]),
-    )))
-    print(multibar((
-        ("*", mix_status["ppv"]),
-        ("-", mix_status["pdisCharge1"]),
-        ("<", mix_status["pactouser"]),
-    )))
-    print()
+        print()
+        print(multibar((("=", soc-10),), 90))
+        print(multibar((
+            ("#", mix_status["pLocalLoad"]),
+            ("+", mix_status["chargePower"]),
+            (">", mix_status["pactogrid"]),
+        )))
+        print(multibar((
+            ("*", mix_status["ppv"]),
+            ("-", mix_status["pdisCharge1"]),
+            ("<", mix_status["pactouser"]),
+        )))
+        print()
 
-    print(bcolors.end, flush=True)
+        print(bcolors.end, flush=True)
 
-    running = True
-    thread.start()
+        running = True
+        thread.start()
 
-    try:
         time.sleep(3*60)
+
+        running = False
+        thread.join()
+        led_set(False)
+        os.system("clear")
+
     except KeyboardInterrupt:
         shutdown()
 
-    running = False
-    thread.join()
-    led_set(False)
-    os.system("clear")
+    except Exception as e:
+        log("error: " + str(e))
+
+        running = False
+        if thread: thread.join()
+        led_set(False)
+
+        try:
+            for _ in range(10*60):
+                time.sleep(1)
+        except KeyboardInterrupt:
+            shutdown()
